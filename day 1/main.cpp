930db82b87325af4a9015d5ab01e332689c4cb1f
Babbaj <babbaj45@gmail.com>
2019-12-04 17:19:38 -0500

    day 1: part 2

--- day 1/main.cpp
+++ day 1/main.cpp
@@ -3,13 +3,16 @@
 #include <string>
 #include <fstream>
 #include <numeric>
+#include <algorithm>
 #include <type_traits>
 
-using namespace std::string_literals;
 
 template<typename F>
 auto readLines(const std::string& fileName, F parser) {
     std::ifstream file(fileName);
+    if (!file) {
+        throw std::runtime_error{"Failed to open input file"};
+    }
 
     std::vector<std::invoke_result_t<F, std::string>> vec;
     std::string line;
@@ -21,13 +24,24 @@ auto readLines(const std::string& fileName, F parser) {
     return vec;
 }
 
-namespace part1 {
-    int64_t fuel(int64_t mass) {
-        return (mass / 3) - 2;
-    }
+template<typename T, typename F>
+auto map(const std::vector<T> vec, F func) {
+    using U = decltype(func(std::declval<T>()));
+    std::vector<U> out;
+    out.reserve(vec.size());
+    std::transform(vec.begin(), vec.end(), std::back_inserter(out), std::move(func));
 
+    return out;
+}
+
+int fuel(int mass) {
+    return (mass / 3) - 2;
+}
+
+namespace part1 {
     void run() {
-        const auto lines = readLines("../input1.txt"s, [](const std::string& str) { return fuel(std::stoi(str)); });
+        const auto lines = readLines("../input1.txt", [](const std::string& str) { return std::stoi(str); });
+        const auto computed = map(lines, fuel);
 
         const auto sum = std::accumulate(lines.begin(), lines.end(), 0ll);
 
@@ -36,12 +50,27 @@ namespace part1 {
 }
 
 namespace part2 {
+    int recursiveFuel(int mass) {
+        if (mass <= 8) return 0;
 
+        const auto f = fuel(mass);
+        return f + recursiveFuel(f);
+    }
+
+
+    void run() {
+        const auto masses = readLines("../input2.txt", [](const std::string& str) { return std::stoi(str); });
+        const std::vector<int> computed = map(masses, recursiveFuel);
+
+        const auto sum = std::accumulate(computed.begin(), computed.end(), 0ll);
+
+        std::cout << sum << '\n';
+    }
 }
 
 
 int main() {
-
+    part2::run();
 }
 
 
